3f17d523ea1dcb9fac9fcce330caa39129527be9
douglasgalvao <douglasgmachado98@gmail.com>
2022-10-05 23:47:01 -0300

    Add a Listener to consume messages from queue in RabbitMQ that's up!

---
+++ src/main/java/estoque/consume/consumer/EstoqueConsumer.java
@@ -0,0 +1,28 @@
+package estoque.consume.consumer;
+
+import dto.PrecoDTO;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import com.microservice.inventory_price.constants.RabbitMQConstants;
+import org.springframework.stereotype.Component;
+import dto.EstoqueDTO;
+
+@Component
+public class EstoqueConsumer {
+
+
+    @RabbitListener(queues = RabbitMQConstants.FILA_ESTOQUE)
+    private void consumer(EstoqueDTO estoqueDTO){
+        System.out.println(estoqueDTO.codigoProduto);
+        System.out.println(estoqueDTO.quantidade);
+        System.out.println("-------------------");
+    }
+
+
+    @RabbitListener(queues = RabbitMQConstants.FILA_PRECO)
+    private void consumer(PrecoDTO precoDTO){
+        System.out.println(precoDTO.codigoProduto);
+        System.out.println(precoDTO.preco);
+        System.out.println("-------------------");
+    }
+
+}
