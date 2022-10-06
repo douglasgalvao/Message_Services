d9fcc15932ea23af1dc44200ef6337e7d6cddae0
douglasgalvao <douglasgmachado98@gmail.com>
2022-10-05 21:53:17 -0300

    Add Estoque and Preco controllers to send message when the enpoint is called

--- src/main/java/com/microservice/inventory_price/controller/EstoqueController.java
+++ src/main/java/com/microservice/inventory_price/controller/EstoqueController.java
@@ -1,2 +1,26 @@
-package com.microservice.inventory_price.controller;public class EstoqueController {
+package com.microservice.inventory_price.controller;
+
+import com.microservice.inventory_price.dto.EstoqueDTO;
+import com.microservice.inventory_price.service.RabbitmqService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "estoque")
+public class EstoqueController {
+
+    @Autowired
+    private RabbitmqService rabbitmqService;
+
+    @PostMapping
+    private ResponseEntity alteraEstoque(@RequestBody EstoqueDTO estoqueDTO){
+        System.out.println(estoqueDTO.codigoProduto);
+        this.rabbitmqService.enviaMensagem(com.microservice.inventory_price.constants.RabbitMQConstants.FILA_ESTOQUE,estoqueDTO);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
--- src/main/java/com/microservice/inventory_price/controller/PrecoController.java
+++ src/main/java/com/microservice/inventory_price/controller/PrecoController.java
@@ -1,28 +1,24 @@
 package com.microservice.inventory_price.controller;
-
 import com.microservice.inventory_price.constants.RabbitMQConstants;
-import com.microservice.inventory_price.dto.EstoqueDTO;
+import com.microservice.inventory_price.dto.PrecoDTO;
 import com.microservice.inventory_price.service.RabbitmqService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
-@RequestMapping(value = "estoque")
+@RequestMapping(value = "preco")
 @RestController
 public class PrecoController {
 
     @Autowired
     private RabbitmqService rabbitmqService;
 
-    @GetMapping
-    private ResponseEntity alteraEstoque(@RequestBody EstoqueDTO estoqueDTO){
-        System.out.println(estoqueDTO.codigoProduto);
-        this.rabbitmqService.enviaMensagem(RabbitMQConstants.FILA_ESTOQUE,estoqueDTO);
+    @PostMapping
+    private ResponseEntity alteraPreco(@RequestBody PrecoDTO precoDTO){
+        System.out.println(precoDTO.codigoProduto);
+        this.rabbitmqService.enviaMensagem(RabbitMQConstants.FILA_PRECO,precoDTO);
         return new ResponseEntity(HttpStatus.OK);
     }
 
