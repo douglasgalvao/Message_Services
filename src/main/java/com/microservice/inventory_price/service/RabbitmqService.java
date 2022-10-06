4cf1e888c7c72b7477f8218b897ae33c7e385303
douglasgalvao <douglasgmachado98@gmail.com>
2022-10-05 21:56:36 -0300

    Add Service that calls convertAndSend from RabbitTemplate

--- src/main/java/com/microservice/inventory_price/service/RabbitmqService.java
+++ src/main/java/com/microservice/inventory_price/service/RabbitmqService.java
@@ -1,4 +1,15 @@
 package com.microservice.inventory_price.service;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RabbitmqService {
+    @Autowired
+    public RabbitTemplate rabbitTemplate;
+
+    public void enviaMensagem(String nomeFila, Object mensagem) {
+        this.rabbitTemplate.convertAndSend(nomeFila, mensagem);
+    }
 }
