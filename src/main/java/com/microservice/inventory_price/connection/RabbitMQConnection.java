2313fbc8866d07d496af686e3ee396d022f064fb
douglasgalvao <douglasgmachado98@gmail.com>
2022-10-05 21:46:42 -0300

    Add connection and a function to test the persisting message in the queue

--- src/main/java/com/microservice/inventory_price/connection/RabbitMQConnection.java
+++ src/main/java/com/microservice/inventory_price/connection/RabbitMQConnection.java
@@ -1,15 +1,24 @@
 package com.microservice.inventory_price.connection;
 
-import com.microservice.inventory_price.contants.RabbitMQConstants;
+import com.microservice.inventory_price.constants.RabbitMQConstants;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class RabbitMQConnection {
 
     private static final String NOME_EXCHANGE = "amq.direct";
+
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin) {
+        this.amqpAdmin = amqpAdmin;
+    }
 
     private Queue fila(String nomeFila) {
         return new Queue(nomeFila, true, false, false);
@@ -20,15 +29,26 @@ public class RabbitMQConnection {
     }
 
     private Binding relacionamento(Queue fila, DirectExchange troca) {
-        return new Binding(fila.getName(), Binding.DestinationType.EXCHANGE, troca.getName(), fila.getName(), null);
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, troca.getName(), fila.getName(), null);
     }
 
+    @PostConstruct
     private void adiciona() {
         Queue filaEstoque = this.fila(RabbitMQConstants.FILA_ESTOQUE);
         Queue filaPreco = this.fila(RabbitMQConstants.FILA_PRECO);
 
         DirectExchange troca = this.trocaDireta();
-        Binding relaciona = this.relacionamento(filaEstoque, troca);
+        Binding ligacaoEstoque = this.relacionamento(filaEstoque, troca);
+        Binding ligacaoPreco = this.relacionamento(filaPreco, troca);
+
+
+        // Criando as filas no RabbitMQ
+        this.amqpAdmin.declareQueue(filaEstoque);
+        this.amqpAdmin.declareQueue(filaPreco);
+        this.amqpAdmin.declareExchange(troca);
+        this.amqpAdmin.declareBinding(ligacaoEstoque);
+        this.amqpAdmin.declareBinding(ligacaoPreco);
+
     }
 
 }
